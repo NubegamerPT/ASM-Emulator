1cc1dc6ccda25895a182a1c75f8e84da0f7386d0
Nubegamer_PT <afonsoclerigomendessousa@gmail.com>
2024-09-28 04:14:18 +0100

    Example File

---
+++ bin/main.s
@@ -0,0 +1,18 @@
+addi a8 a8 2
+subb b8 a8 1
+push a8
+push b8
+addi a8 a8 6
+addi b8 a8 2
+pop b8
+addi b8 a8 0
+pop a8
+load a8 0
+load b8 0
+addi a8 a8 1
+addi b8 b8 1
+store a8 0
+push b8
+addi b8 b8 5
+push b8
+halt
